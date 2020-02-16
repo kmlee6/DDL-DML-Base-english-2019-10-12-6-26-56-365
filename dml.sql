35c91fac677ccdec0e8d87f2f932ee0eb0a48607
kmlee6 <32027946+kmlee6@users.noreply.github.com>
2020-02-16 15:20:58 +0800

    Add files via upload

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into table training values ("001", "1001")
 -- Revise record
-
---  Delete record
-
+update training set sid=3 where sid=1;
+-- Delete record
+delete from training where sid=3;
 -- Search record
+select * from training;
